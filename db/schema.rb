b23f27b2d897d51804ba8767983e7f79fd928c1a
Ze MrRipley <dagschotel@hotmail.com>
2016-02-25 09:07:21 +0000

    DB fix

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160218170537) do
+ActiveRecord::Schema.define(version: 20160225090450) do
 
   create_table "contacts", force: true do |t|
     t.string   "name"
@@ -43,7 +43,6 @@ ActiveRecord::Schema.define(version: 20160218170537) do
     t.datetime "updated_at"
     t.integer  "plan_id"
     t.string   "stripe_customer_token"
-    t.string   "strip_customer_token"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
